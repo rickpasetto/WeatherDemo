6e40aa31ec80dccfc205d8abbfc81f94decfa612
Rick Pasetto <rick.pasetto@joby.aero>
2025-12-21 11:36:10 -0800

    Initial Commit

---
+++ WeatheriOS/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Rick Pasetto on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatheriOS/WeatherTests/WeatherTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Rick Pasetto on 12/21/25.
+//
+
+import Testing
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
